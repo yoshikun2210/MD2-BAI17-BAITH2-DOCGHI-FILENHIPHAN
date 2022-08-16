e3e95c3355890c834e916d25e370325d36e3de40
yoshinkun2210 <yoshimkun22@gmail.com>
2022-08-17 01:10:59 +0700

    th

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String PATH_FILE = "D:\\MD2\\untitled75\\src\\student.txt";
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Trịnh Đức Toàn", "Hà Nội"));
+        students.add(new Student(2, "Lê Việt Dũng", "Phú Thọ"));
+        students.add(new Student(3, "Phùng Trung Dũng", "Đà Nẵng"));
+        students.add(new Student(4, "Nguyễn Khánh Tùng", "Hà Nội"));
+        students.add(new Student(5, "Nguyễn Đồng Chính", "Hải Dương"));
+
+        writeToFile(PATH_FILE, students);
+        List<Student> studentDataFromFile = readDataFromFile(PATH_FILE);
+        for (Student student : studentDataFromFile){
+            System.out.println(student);
+        }
+    }
+
+    public static void writeToFile(String path, List<Student> students) {
+        try {
+            FileOutputStream fos = new FileOutputStream(path);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(students);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Student> readDataFromFile(String path){
+        List<Student> students = new ArrayList<>();
+        try{
+            FileInputStream fis = new FileInputStream(path);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            students = (List<Student>) ois.readObject();
+            fis.close();
+            ois.close();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return students;
+    }
+}
